23f442518a4fc719116af426fd78189e47617f45
José Felipe Ribeiro <felipe.brx.dev@gmail.com>
2021-12-29 09:52:53 -0300

    produtos update 1.3

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import produtos_list,Produto_update
+from .views import produtos_list,Produto_update, Produto_Novo, Produto_Delete
 
 
 urlpatterns = [
     path('list/', produtos_list, name="produto_list"),
-path('update/<int:id>/', Produto_update, name="produto_update"),
 
+    path('novo/', Produto_Novo, name="Produto_Novo"),
+    path('update/<int:id>/', Produto_update, name="produto_update"),
+    path('delete/<int:id>/', Produto_Delete, name="Produto_Delete"),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -20,3 +20,24 @@ def Produto_update(request, id):
         return redirect('produto_list')
 
     return render(request, 'produtos/produto_update.html', {'form': form})
+
+@login_required
+def Produto_Novo(request):
+    produto_id = get_object_or_404(Produto, pk=id)
+    form = ProdutoForm(request.POST or None, request.FILES or None, instance=produto_id)
+
+    if form.is_valid():
+        form.save()
+        return redirect('produto_list')
+
+    return render(request, 'produtos/produto_update.html', {'form': form})
+
+@login_required
+def Produto_Delete(request, id):
+    person = Produto.objects.using('default').get(id=id)
+
+    if request.method == 'POST':
+        person.delete(using='default')
+        return redirect('person_list')
+
+    return render(request, 'produto_delete.html', {'person': person})
